cb736d387a18fef708ccf141b0d2e849904a02cc
Vahab Mehdiyev <mehdiev.v@advers.ru>
2025-09-25 15:42:22 +0400

    check xxxx  develop

--- api/device/device_service.py
+++ api/device/device_service.py
@@ -2,5 +2,5 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from .device_dao import list_devices
 
 async def get_devices(db: AsyncSession) -> list[dict]:
-    # Biznes qaydalarını sonra bura əlavə edəcəyikk
+    # Biznes qaydalarını sonra bura əlavə edəcəyikkk
     return await list_devices(db)
